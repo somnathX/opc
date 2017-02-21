848f4495754edb2de06fb8558d6029386f2da4d3
Nitin sharma <13etccs025@techninjr.org>
2017-02-21 17:50:26 +0530

    Create structure

--- compile.php
+++ compile.php
@@ -5,17 +5,82 @@
 		private $error;
 		private $input;
 		private $output;
-		private $language_id;
-		private $source_code;
-		const $url = "https://api.judge0.com/submissions/";
+		private $languageId;
+		private $sourceCode;
+		const url = "https://api.judge0.com/submissions/";
 
-		function __construct($source_code,$language_id,$input) {
+		function __construct($source_code,$language_id,$input,$output,$token,$error) {
    			$this->source_code = $source_code; //from input
    			$this->language_id = $language_id; //from input
    			$this->input = $input;//from input
+   			$this->output = $output;
+   			$this->token = $token;
+   			$this->error = $error;
 		}
 
 		function getToken()
+		{
+			return $this->token;
+		}
+
+		function setToken($token)
+		{
+			$this->token = $token;
+		}
+
+		function getInput()
+		{
+			return $this->input;
+		}
+
+		function setInput($input)
+		{
+			$this->input = $input;
+		}
+
+		function getOutput()
+		{
+			return $this->output;
+		}
+
+		function setOutput($output)
+		{
+			$this->output = $output;
+		}
+
+		function getSourceCode()
+		{
+			return $this->sourceCode ;
+		}
+
+		function setSourceCode($sourceCode)
+		{
+			$this->sourceCode = $sourceCode;
+		}
+
+
+		function getLanguageID()
+		{
+			return $this->languageId;
+		}
+
+		function setLanguageId($languageId)
+		{
+			$this->languageId = $languageId;
+		}
+
+		function getError()
+		{
+			return $this->error;
+		}
+
+		function setError($error)
+		{
+			$this->error = $error;
+		}
+
+
+		function createToken()
 		{
 			$data = Array("source_code"=>$source_code,
 				"language_id"=>$language_id,"input"=>$input);
@@ -37,5 +102,5 @@
 
 
 	}
-	
+
 ?>
