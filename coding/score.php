f329c2c04eb84574199b6f1dddc08ad088d8d4e7
Nitin sharma <13etccs025@techninjr.org>
2017-03-02 18:40:58 +0530

    Add score File

---
+++ coding/score.php
@@ -0,0 +1,69 @@
+<?php
+	include("C:/xampp/htdocs/opc/controller/connection.php");
+
+	function calculateScore($POST)
+	{
+		$conn = createConnection();
+		$query = "SELECT `input`, `output` FROM `problems` WHERE pid = " . $POST['pid'];
+		$result = mysqli_query($conn , $query);
+		$response = array("score"=>'0');
+		if(!$result)
+		{
+			$response['score'] = 0;
+			return ;
+		}
+		else
+		{
+			$row = mysqli_fetch_array($result,MYSQLI_NUM);
+			$input = $row[0];
+			$output = $row[1];
+			echo $input;
+			$source_code = $POST['source_code'];
+			$language_id = $POST['language_id'];
+			$request = array("source_code"=>$source_code,"language_id"=>$language_id,
+				"input"=>$input,"expected_output"=>$output);
+			$request = json_encode($request);
+			//echo $request;
+			$options = array(
+  				'http' => array(
+  					'user_agent' => 'custom user agent string',
+    				'header'  => array(
+    					'Content-Type: application/json',
+						'Accept : application/json',
+					),
+    				'method'  => 'POST',
+    				'content' => $request,
+  				),
+			);
+			$url = "http://localhost/opc/controller/output.php";
+			$context  = stream_context_create($options);
+			$result = file_get_contents($url,false, $context);
+			if(!$result)
+			{
+				$response["score"] = 0;
+
+			}
+			else
+			{
+				$result = json_decode($result,true);
+				$error = $result["error"];
+				if(!empty($error))
+				{
+					$response["error"] = "Wrong Answer";
+					$response["output"] = $result["output"];
+				}
+				else
+				{
+					$response["score"] = "20";
+					$response["output"] = $result["output"];
+				}
+			}
+			return $response;
+		}
+	}
+
+	$POST = file_get_contents('php://input');
+	$POST = json_decode($POST,true);
+	$response = calculateScore($POST);
+	echo json_encode($response);
+?>
